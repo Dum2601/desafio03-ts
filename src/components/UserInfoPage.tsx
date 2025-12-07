b5259377009eaaea81450bf56f4f8b37e4e82ebe
dum2601 <doug_b_souza@hotmail.com>
2025-12-07 19:30:39 -0300

    logica da pagina de informacoes do usuario

--- src/components/UserInfoPage.tsx
+++ src/components/UserInfoPage.tsx
@@ -1,10 +1,31 @@
-const UserInfoUser = () =>
+import { Box } from '@chakra-ui/react'
+interface UserProps
+{
+
+    nome: string
+    email: string
+
+}
+
+const UserInfoUser = ({nome, email}: UserProps) =>
 {
 
     return (
 
-        <></>
+        <>
+        
+            <Box>
+
+                {nome}
+                {email}
+
+            </Box>
+
+        </>
 
     )
 
 }
+
+
+export default UserInfoUser
---
+++ src/pages/UserInfo.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@chakra-ui/react'
+import UserInfoPage from '../components/UserInfoPage'
+
+const UserInfo = () =>
+{
+
+    return (
+
+        <>
+        
+            <Box>
+
+                < UserInfoPage nome='Name' email='email' />
+
+            </Box>
+
+        </>
+
+    )
+
+}
+
+export default UserInfo
